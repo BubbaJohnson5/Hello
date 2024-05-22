391dde6621979894a26efcb32e98c39f89b09628
DanSchweiger <daniel.schweiger@psgglobal.net>
2024-05-21 21:37:33 -0500

    More files for the Hello program

---
+++ Hello.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+
+
+int main()
+{
+	printf( "Hello World, again, Ver=3\n" );
+
+	printf( "Another printf Hello World Ver=3\n" );
+
+	printf( "More printf's\n");
+
+	return( 0 );
+}
